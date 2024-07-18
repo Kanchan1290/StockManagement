197d68dabcf40042aa6a7b6d8c94fd6965d2fb18
Kanchan1290 <kadelkanchan4@gmail.com>
2024-07-18 15:35:07 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using StockManagementSystem.Models;
+using StockManagementSystem.Security;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddSingleton<DataSecurityKey>();
+        builder.Services.AddDbContext<StockmanagementContext>(o => o.UseSqlServer(builder.Configuration["Conn"]));
+
+
+        var app = builder.Build();
+
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
